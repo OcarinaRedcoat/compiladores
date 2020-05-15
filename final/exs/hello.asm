400c45adba8eba189c0c07ef93d8b7a99a24b2b3
OcarinaRedcoat <98.ricardo.caetano@gmail.com>
2020-05-15 16:44:20 +0100

    resolved conflict

---
+++ final/exs/hello.asm
@@ -0,0 +1,34 @@
+; GLOBL
+global	$_main:function
+; TEXT
+segment	.text
+; ALIGN
+align	4
+; LABEL
+$_main:
+; ENTER
+	push	ebp
+	mov	ebp, esp
+	sub	esp, 0
+; RODATA
+segment	.rodata
+; ALIGN
+align	4
+; LABEL
+$_L1:
+; CHAR
+	db	0xC6BDFF0
+; EXTRN
+extern	$_prints
+; CALL
+	call	$_prints
+; TRASH
+	add	esp, 3
+; IMM
+	push	dword 0
+; POP
+	pop	eax
+; LEAVE
+	leave
+; RET
+	ret
