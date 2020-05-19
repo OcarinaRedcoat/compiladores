346df6c43b8ba7cdb5c9958cae1d951389cc87e7
OcarinaRedcoat <98.ricardo.caetano@gmail.com>
2020-05-19 23:32:47 +0100

    = and ~= in strings

---
+++ final/run/minorhelp.c
@@ -0,0 +1,26 @@
+int _power(int base, int pow) { 
+	if (pow == 0) return 1;
+	int c = pow;
+	int res = 1;
+	while (c != 0){
+		res = res * base;
+		c = c - 1;
+	}
+	return res;
+}
+
+int _strcmp(char* firstString,char* secondString){
+	while(*firstString && (*firstString == *secondString)) {
+		firstString++;
+		secondString++;
+	}
+	return *(unsigned char*)firstString == *(unsigned char*)secondString;
+}
+
+int _strncmp(char* firstString,char* secondString){
+	while(*firstString && (*firstString == *secondString)) {
+		firstString++;
+		secondString++;
+	}
+	return *(unsigned char*)firstString != *(unsigned char*)secondString;
+}
